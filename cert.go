ff21a0da12adc6c20c8bc0bfb4819b4da0ddff8c
Jesse Redl <jredl@vendasta.com>
2019-01-25 13:04:03 -0600

    Chrome doesn't handle the private key generated via mitm. Fork and modify.

--- cert.go
+++ cert.go
@@ -63,7 +63,7 @@ func genCert(ca *tls.Certificate, names []string) (*tls.Certificate, error) {
 }
 
 func genKeyPair() (*ecdsa.PrivateKey, error) {
-	return ecdsa.GenerateKey(elliptic.P521(), rand.Reader)
+	return ecdsa.GenerateKey(elliptic.P256(), rand.Reader)
 }
 
 func GenCA(name string) (certPEM, keyPEM []byte, err error) {
@@ -75,9 +75,9 @@ func GenCA(name string) (certPEM, keyPEM []byte, err error) {
 		NotAfter:              now.Add(caMaxAge),
 		KeyUsage:              caUsage,
 		BasicConstraintsValid: true,
-		IsCA:               true,
-		MaxPathLen:         2,
-		SignatureAlgorithm: x509.ECDSAWithSHA512,
+		IsCA:                  true,
+		MaxPathLen:            2,
+		SignatureAlgorithm:    x509.ECDSAWithSHA512,
 	}
 	key, err := genKeyPair()
 	if err != nil {
